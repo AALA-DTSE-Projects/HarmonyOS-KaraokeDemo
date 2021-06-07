8a379dd378a3885004ff5c3378e4935de147d3a4
Tran Thanh Huy 00564191 <tran.thanh.huy@huawei.com>
2021-06-07 19:49:29 +0900

    Add general sign build script

---
+++ scripts/buildAndSignHaps.bat
@@ -0,0 +1,10 @@
+@echo off
+cd ..
+call gradlew.bat clean
+call gradlew.bat aD
+cd build\outputs\hap\debug
+set current_dir="%cd%"
+for /r %current_dir% %%f in (*unsigned.hap) do (
+    call demoSignHap.bat %%f
+    timeout /t 5
+)
