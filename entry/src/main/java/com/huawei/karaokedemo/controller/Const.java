d2344547e96b49f2ff10ad40216d588e86cfad03
Tran Thanh Huy 00564191 <tran.thanh.huy@huawei.com>
2021-06-07 11:01:17 +0900

    Increase the buffer size, update the ohos version

--- entry/src/main/java/com/huawei/karaokedemo/controller/Const.java
+++ entry/src/main/java/com/huawei/karaokedemo/controller/Const.java
@@ -5,5 +5,5 @@ public class Const {
     public static final String ABILITY_NAME = "com.huawei.karaokedemo.ControllerServiceAbility";
     public static final String DEVICE_ID_KEY = "deviceId";
     public static final int AUDIO_SAMPLE_RATE = 44100;
-    public static final int BUFFER_SIZE = 1024; // 1KB
+    public static final int BUFFER_SIZE = 10240; // 10KB
 }
