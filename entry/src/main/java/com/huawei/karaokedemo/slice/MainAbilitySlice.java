b970074e910ee085a3cd5d486037e29b569b0453
Tran Thanh Huy 00564191 <tran.thanh.huy@huawei.com>
2021-06-23 18:23:12 +0900

    Change to use STEREO and MEDIA record mode

--- entry/src/main/java/com/huawei/karaokedemo/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/huawei/karaokedemo/slice/MainAbilitySlice.java
@@ -184,9 +184,9 @@ public class MainAbilitySlice extends AbilitySlice {
         AudioStreamInfo audioStreamInfo =
                 new AudioStreamInfo.Builder()
                         .encodingFormat(AudioStreamInfo.EncodingFormat.ENCODING_PCM_16BIT)
-                        .channelMask(AudioStreamInfo.ChannelMask.CHANNEL_IN_MONO)
+                        .channelMask(AudioStreamInfo.ChannelMask.CHANNEL_IN_STEREO)
                         .sampleRate(Const.AUDIO_SAMPLE_RATE)
-                        .streamUsage(AudioStreamInfo.StreamUsage.STREAM_USAGE_VOICE_COMMUNICATION)
+                        .streamUsage(AudioStreamInfo.StreamUsage.STREAM_USAGE_MEDIA)
                         .build();
         AudioCapturerInfo audioCapturerInfo =
                 new AudioCapturerInfo.Builder()
